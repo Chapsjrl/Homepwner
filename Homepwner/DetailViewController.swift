b8bfce24d001cbf33474b8d22c82227c9e32227f
Chapsjrl <jalerivera@live.com.mx>
2018-04-28 10:51:53 -0500

    Model for store image

--- Homepwner/DetailViewController.swift
+++ Homepwner/DetailViewController.swift
@@ -74,6 +74,17 @@ class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationC
         present(imagePicker, animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String: Any]) {
+        // Get picked image from info dictionary
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        // Put that image on the screen in the image view
+        imageView.image = image
+        // Take image picker off the screen -
+        // you must call this dismiss method
+        dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
     }
